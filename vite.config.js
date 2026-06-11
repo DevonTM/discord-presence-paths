f37ef60504592485bac8505eddc399aff0cca4b0
DevonTM <ipangcheater@gmail.com>
2026-06-12 00:21:20 +0700

    chore: add sqlite content type plugin to Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite';
 
+const sqliteContentTypePlugin = () => ({
+  name: 'sqlite-content-type',
+  configureServer(server) {
+    server.middlewares.use((req, res, next) => {
+      if (req.url?.split('?')[0] === '/detectable.db') {
+        res.setHeader('Content-Type', 'application/vnd.sqlite3');
+      }
+      next();
+    });
+  },
+  configurePreviewServer(server) {
+    server.middlewares.use((req, res, next) => {
+      if (req.url?.split('?')[0] === '/detectable.db') {
+        res.setHeader('Content-Type', 'application/vnd.sqlite3');
+      }
+      next();
+    });
+  },
+});
+
 export default defineConfig({
-  plugins: [],
+  plugins: [sqliteContentTypePlugin()],
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
